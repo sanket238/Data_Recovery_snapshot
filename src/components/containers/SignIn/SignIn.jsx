ab62f7cf60f7c62b0ccd7481b6a8e287447b3964
mitesh <miteshtagadiya@gmail.com>
2020-04-28 23:20:45 +0530

    SignIn created

---
+++ src/components/containers/SignIn/SignIn.jsx
@@ -0,0 +1,149 @@
+import React, { useState } from "react";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Container,
+  Typography,
+  Grid,
+  Box,
+  Checkbox,
+  FormControlLabel,
+  TextField,
+  CssBaseline,
+  Button,
+  Avatar,
+  Card,
+} from "@material-ui/core";
+import { Link, withRouter } from "react-router-dom";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" to="/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  container: {
+    height: "100vh",
+    justifyContent: "center",
+    flexDirection: "column",
+    display: "flex",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  card: {
+    margin: theme.spacing(-5),
+    padding: theme.spacing(5),
+  },
+}));
+
+const SignIn = (props) => {
+  const classes = useStyles();
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
+  const onLogin = (e) => {
+    e.preventDefault();
+    localStorage.setItem("username", inputs.email);
+    props.history.push("/dashboard");
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.container}>
+        <Card className={classes.card}>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form onSubmit={onLogin} className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                onChange={handleChange}
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                onChange={handleChange}
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link to="/forgot-password" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Typography variant="body2">
+                    Don't have an account? <Link to="/signup">Sign Up</Link>
+                  </Typography>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+
+          <Box mt={8}>
+            <Copyright />
+          </Box>
+        </Card>
+      </div>
+    </Container>
+  );
+};
+
+export default withRouter(SignIn);
