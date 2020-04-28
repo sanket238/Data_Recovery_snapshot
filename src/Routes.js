0e874aaa9acc27e7524b1cd1c44d60a7357d34a6
mitesh <miteshtagadiya@gmail.com>
2020-04-28 23:20:22 +0530

    Routes.js file

---
+++ src/Routes.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import SignIn from "./components/containers/SignIn/SignIn";
+import SignUp from "./components/containers/SignUp/SignUp";
+import ForgotPassword from "./components/containers/ForgotPassword/ForgotPassword";
+import App from "./App";
+
+function Routes() {
+  window.addEventListener("storage", (e) => {
+    if (!localStorage.getItem("username")) {
+      window.location.reload();
+    }
+  });
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={App} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/forgot-password" component={ForgotPassword} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
