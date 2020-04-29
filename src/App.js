a7edb07e0419a4ba0292b7bbc553cd4c7715029c
mitesh <miteshtagadiya@gmail.com>
2020-04-29 23:34:59 +0530

    App component changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import SignIn from "./components/containers/SignIn/SignIn";
-import Dashboard from "./components/containers/Dashboard/Dashboard";
+import Home from "./components/containers/Home/Home";
 
 function App() {
   let isLoggedIn = localStorage.getItem("username");
 
-  return <Route>{isLoggedIn === null ? <SignIn /> : <Dashboard />}</Route>;
+  return <Route>{isLoggedIn === null ? <SignIn /> : <Home />}</Route>;
 }
 
 export default App;
