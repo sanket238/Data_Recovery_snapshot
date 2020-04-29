69bd962a418c7a013c2203b445191e8575695be4
mitesh <miteshtagadiya@gmail.com>
2020-04-30 00:28:57 +0530

    LocalStorage changed

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import SignIn from "./components/containers/SignIn/SignIn";
 import Home from "./components/containers/Home/Home";
 
 function App() {
-  let isLoggedIn = localStorage.getItem("username");
+  let isLoggedIn = localStorage.getItem("isLoggedIn");
 
-  return <Route>{isLoggedIn === null ? <SignIn /> : <Home />}</Route>;
+  return (
+    <Route>
+      {isLoggedIn === null || isLoggedIn === false ? <SignIn /> : <Home />}
+    </Route>
+  );
 }
 
 export default App;
