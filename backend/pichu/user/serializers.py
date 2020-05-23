969a48fcb6bd25dd6f63e063726dca806257bf46
Viraj Trivedi <inf3cti0n95@gmail.com>
2020-05-23 15:01:15 +0530

    fix: changed, serializer for User Profile

--- backend/pichu/user/serializers.py
+++ backend/pichu/user/serializers.py
@@ -6,8 +6,8 @@ from django.contrib.auth import get_user_model
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        read_only_fields = ("created_at", "updated_at", "last_login", "username", "email", "id")
-        exclude = ["password", "is_staff", "is_superuser", "is_active", "groups", "user_permissions"]
+        read_only_fields = ("created_at", "updated_at", "last_login", "bio", "is_email_verified", "avatar", "id")
+        exclude = ["is_staff", "is_superuser", "is_active", "groups", "user_permissions"]
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
