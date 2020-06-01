255e65ae40913afa5a89e35a4d43db83e74826ea
mitesh <miteshtagadiya@gmail.com>
2020-06-02 00:08:04 +0530

    Show Directory size in card

--- frontend/src/components/ui/Card/Card.jsx
+++ frontend/src/components/ui/Card/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Paper } from "@material-ui/core";
+import { Grid, Paper, CircularProgress } from "@material-ui/core";
 import {
   VideoLibrary,
   Image,
@@ -11,6 +11,7 @@ import {
   PictureAsPdf
 } from "@material-ui/icons";
 import "./Card.css";
+import { formatBytes } from "../../../utils/utils";
 
 const Card = props => {
   let images = {
@@ -39,28 +40,53 @@ const Card = props => {
 
   return (
     <Grid item xs={12} md={6} lg={4} sm={6}>
-      <Paper
-        elevation={3}
-        style={props.style ? props.style : {}}
-        onClick={() => props.onClick(props.label)}
-        className="dashboard-card"
-      >
-        <div className="card-padding">
-          <div className={classNames[props.icon]}>{images["files"]}</div>
-          <div style={{ marginTop: 15 }}>
-            <label className="card-label">{props.label}</label>
-          </div>
-          <div className="card-number">
-            {/* <label className="card-number-label">{props.value}</label> */}
-            <div
-              style={{ fontSize: 18, fontWeight: "bold", color: "grey" }}
-            ></div>
-            <div style={{ fontSize: 16, fontWeight: "bold", color: "grey" }}>
-              {props.value} Files and {props.directories} Sub Folders
+      {props.label ? (
+        <Paper
+          elevation={3}
+          style={props.style ? props.style : {}}
+          onClick={() => props.onClick(props.label)}
+          className="dashboard-card"
+        >
+          <div className="card-padding">
+            <div className={classNames[props.icon]}>{images["files"]}</div>
+            <div style={{ marginTop: 15 }}>
+              <label className="card-label">{props.label}</label>
+            </div>
+            <div className="card-number">
+              {/* <label className="card-number-label">{props.value}</label> */}
+              <div
+                style={{ fontSize: 18, fontWeight: "bold", color: "grey" }}
+              ></div>
+              <div style={{ fontSize: 16, fontWeight: "bold", color: "grey" }}>
+                {props.value} Files and {props.directories} Sub Folders
+              </div>
+              {typeof props.size !== "undefined" ? (
+                <div
+                  style={{
+                    fontSize: 16,
+                    fontWeight: "bold",
+                    color: "grey",
+                    marginTop: 10
+                  }}
+                >
+                  {formatBytes(props.size)} Recovered
+                </div>
+              ) : null}
             </div>
           </div>
-        </div>
-      </Paper>
+        </Paper>
+      ) : (
+        <Paper
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: 180
+          }}
+        >
+          <CircularProgress />
+        </Paper>
+      )}
     </Grid>
   );
 };
