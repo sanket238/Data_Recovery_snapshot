e5e36e96a92df9d750579a08c40d34fd7d549a82
mitesh <miteshtagadiya@gmail.com>
2020-06-02 23:09:51 +0530

    ExpandPanel responsive fix

--- frontend/src/components/ui/ExpandPanel/ExpandPanel.jsx
+++ frontend/src/components/ui/ExpandPanel/ExpandPanel.jsx
@@ -1,74 +1,13 @@
 import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-import MuiExpansionPanel from "@material-ui/core/ExpansionPanel";
-import MuiExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import MuiExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { Paper, Chip, CircularProgress } from "@material-ui/core";
+import { Paper, Chip, CircularProgress, Button } from "@material-ui/core";
 import { Folder } from "@material-ui/icons";
 import CallMadeIcon from "@material-ui/icons/CallMade";
+import Collapsible from "react-collapsible";
 import { formatBytes } from "../../../utils/utils";
 import Table from "../Table/Table";
-
-const ExpansionPanel = withStyles({
-  root: {
-    // border: "1px solid rgba(0, 0, 0, .125)",
-    boxShadow: "none",
-    "&:not(:last-child)": {
-      borderBottom: 0
-    },
-    "&:first-child": {
-      borderRadius: "10px 10px 0px 0px"
-    },
-    "&:last-child": {
-      borderRadius: "0px 0px 10px 10px"
-    },
-    "&:before": {
-      display: "none"
-    },
-    "&$expanded": {
-      margin: "auto"
-    }
-  },
-  expanded: {}
-})(MuiExpansionPanel);
-
-const ExpansionPanelSummary = withStyles({
-  root: {
-    // backgroundColor: "rgba(0, 0, 0, .03)",
-    // borderBottom: "1px solid rgba(0, 0, 0, .125)",
-    // marginBottom: -1,
-    flexDirection: "row-reverse",
-    padding: 0,
-    minHeight: 56,
-    "&$expanded": {
-      minHeight: 56
-    }
-  },
-  content: {
-    margin: "12px 16px",
-    "&$expanded": {
-      margin: "12px 16px"
-    }
-  },
-  expanded: {}
-})(MuiExpansionPanelSummary);
-
-const ExpansionPanelDetails = withStyles(theme => ({
-  root: {
-    padding: theme.spacing(2),
-    border: "1px solid lightgrey",
-    borderRadius: 10
-  }
-}))(MuiExpansionPanelDetails);
+import "./ExpandPanel.css";
 
 const ExpandPanel = props => {
-  const [expanded, setExpanded] = React.useState("panel1");
-
-  const handleChange = panel => (event, newExpanded) => {
-    setExpanded(newExpanded ? panel : false);
-  };
-
   const renderPanel = (data, index) => {
     const column = [
       { title: "Name", field: "name" },
@@ -96,19 +35,10 @@ const ExpandPanel = props => {
           })
         : [];
     return (
-      <ExpansionPanel key={index} square>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls={index + "-content"}
-          id={index + "-header"}
-        >
-          <div
-            style={{
-              display: "flex",
-              width: "100%",
-              justifyContent: "space-between"
-            }}
-          >
+      <Collapsible
+        key={data.name}
+        trigger={
+          <div className="collapse">
             <div>
               <div style={{ fontSize: 16, fontWeight: "bold", color: "grey" }}>
                 <span>
@@ -133,57 +63,47 @@ const ExpandPanel = props => {
                 />
               </div>
             </div>
-            <div
-              style={{
-                display: "flex",
-                width: 200,
-                justifyContent: "space-between",
-                fontSize: 16,
-                fontWeight: "bold",
-                color: "grey",
-                marginTop: 3
-              }}
-            >
+            <div className="expandpanel-values">
               <span>{formatBytes(data.totalSize)}</span>
               <span>{data.numberOfFiles} Files</span>
             </div>
           </div>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <div style={{ width: "100%" }}>
-            {data.directories.length > 0 &&
-              data.directories.map((data, index) => {
-                return renderPanel(data, index);
-              })}
+        }
+      >
+        <div className="collapse-subcontainer">
+          {data.directories.length > 0 &&
+            data.directories.map((data, index) => {
+              return renderPanel(data, index);
+            })}
+          <div className="collapse-table">
             {data.files.length > 0 && (
               <Table title="" column={column} data={tableData} />
             )}
           </div>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
+          {data.files.length > 0 && (
+            <div className="collapse-details-button">
+              <Button
+                onClick={() => props.onPanelClick(data)}
+                variant="contained"
+                color="primary"
+                size="small"
+              >
+                View Details
+              </Button>
+            </div>
+          )}
+        </div>
+      </Collapsible>
     );
   };
+
   return (
-    <div style={{ width: "100%" }}>
+    <div className="ExpandPanel" style={{ width: "100%" }}>
       <Paper style={{ borderRadius: 10, padding: 15 }}>
         {props.data.length !== 0 ? (
-          <ExpansionPanel
-            style={{ borderTopLeftRadius: 10, borderTopRightRadius: 10 }}
-            key={props.data.drive}
-            square
-          >
-            <ExpansionPanelSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls={props.data.drive + "-content"}
-              id={props.data.drive + "-header"}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  width: "100%",
-                  justifyContent: "space-between"
-                }}
-              >
+          <Collapsible
+            trigger={
+              <div className="collapse">
                 <div>
                   <div
                     style={{ fontSize: 16, fontWeight: "bold", color: "grey" }}
@@ -210,31 +130,20 @@ const ExpandPanel = props => {
                     />
                   </div>
                 </div>
-                <div
-                  style={{
-                    display: "flex",
-                    width: 200,
-                    justifyContent: "space-between",
-                    fontSize: 16,
-                    fontWeight: "bold",
-                    color: "grey",
-                    marginTop: 3
-                  }}
-                >
+                <div className="expandpanel-values">
                   <span>{formatBytes(props.data.totalSize)}</span>
                   <span>{props.data.totalFiles} Files</span>
                 </div>
               </div>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <div style={{ width: "100%" }}>
-                {props.data.length !== 0 &&
-                  props.data.directories.map((data, index) => {
-                    return renderPanel(data, index);
-                  })}
-              </div>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
+            }
+          >
+            <div className="collapse-subcontainer">
+              {props.data.length !== 0 &&
+                props.data.directories.map((data, index) => {
+                  return renderPanel(data, index);
+                })}
+            </div>
+          </Collapsible>
         ) : (
           <div
             style={{
